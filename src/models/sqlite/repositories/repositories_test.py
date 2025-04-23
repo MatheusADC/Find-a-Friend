c9bb414e01904492aa04c30806d8c822bc58567b
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2025-04-22 22:21:42 -0300

    Add: Test to insert person

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,6 +1,7 @@
 import pytest
 from src.models.sqlite.settings.connection import db_connection_handler
 from .pets_repository import PetsRepository
+from .people_repository import PeopleRepository
 
 db_connection_handler.connect_to_db()
 
@@ -16,3 +17,13 @@ def test_delete_pet():
     name = "shrek"
     repo = PetsRepository(db_connection_handler)
     repo.delete_pets(name)
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_insert_people():
+    first_name = "test name 2"
+    last_name = "test last 2"
+    age = 29
+    pet_id = 2
+
+    repo = PeopleRepository(db_connection_handler)
+    repo.insert_person(first_name, last_name, age, pet_id)
